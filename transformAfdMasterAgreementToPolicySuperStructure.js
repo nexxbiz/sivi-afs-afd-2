a838e75ce06fb1e638d14c8ae6a7ef3f9493552d
Omid Tansaz <omid.tansaz@nexxbiz.io>
2023-08-19 07:45:23 +0200

    Create transformAfdMasterAgreementToPolicySuperStructure.js

---
+++ transformAfdMasterAgreementToPolicySuperStructure.js
@@ -0,0 +1,30 @@
+
+function transformAfdMasterAgreementToPolicySuperStructure(source) {
+    // Step 1 & 2: Extract policies from masterAgreement and move to root
+    let extractedPolicies = [];
+    let extractedParties = source.party ? [...source.party] : [];  // Initialize with root parties if they exist
+    
+    source.masterAgreement.forEach(ma => {
+        // Extract policies and add masterAgreementRef
+        ma.policy.forEach(p => {
+            p.masterAgreementRef = [ma.refKey];
+            extractedPolicies.push(p);
+        });
+        // Generate policyRef for masterAgreement
+        ma.policyRef = ma.policy.map(p => p.refKey);
+        // Remove the policy from masterAgreement as it's now at root
+        delete ma.policy;
+
+        // Check and extract parties
+        if (ma.party) {
+            extractedParties = extractedParties.concat(ma.party);
+            delete ma.party;
+        }
+    });
+
+    // Add extracted policies and parties to the root
+    source.policy = extractedPolicies;
+    source.party = extractedParties;
+
+    return source;
+}
